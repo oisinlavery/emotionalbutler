0092e717d963588c954bde5fb2ac5ea9d0d3aa37
Oisin Lavery <oisinlavery@google.com>
2016-04-21 10:59:50 -0700

    no message

--- index.js
+++ index.js
@@ -139,25 +139,12 @@ function sendGifs() {
           })
         }
 
-        messageData = {
+        var messageData = {
           "attachment": {
               "type": "template",
               "payload": {
                   "template_type": "generic",
-                  "elements": [{
-                      "title": "First card",
-                      "subtitle": "Element #1 of an hscroll",
-                      "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                      "buttons": [{
-                          "type": "web_url",
-                          "url": "https://www.messenger.com",
-                          "title": "web url"
-                      }, {
-                          "type": "postback",
-                          "title": "Postback",
-                          "payload": "Payload for first element in a generic bubble",
-                      }],
-                  }
+                  "elements": elements
               }
           }
         }
