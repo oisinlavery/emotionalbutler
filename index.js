c8ba0ea9b1dc15443b08ce597f143fdc13bbb15d
Oisin Lavery <oisinlavery@google.com>
2016-04-24 12:29:48 -0700

    might be broken, updating password though

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var app = express()
-
+var rx = require('rx')
 var giphy = require('giphy-api')()
 
 var token = "EAAHFrm1kIZAABAPYZBPDLi32BizaAoMaXMLWUFX0DK2yZC1wnG6Ax5SWHjTzD2nsNb3GCKtQN4bGLqEJud6kzCFxTXJZBXYzBMFEc8SSavHI1hvh4JHptHAJxZBgLvi6NItZCrJIIUVZB4llQhvKli5QYqFd875nkILDiFddjKmrwZDZD"
+
+var app = express()
+
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -22,7 +24,7 @@ app.get('/', function(req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function(req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'I_am_your_password_peeXqcJcPV48dZ') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -33,12 +35,11 @@ app.post('/webhook/', function(req, res) {
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
+
         if (event.message && event.message.text) {
             text = event.message.text
 
             sendGifs(text)
-            
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
@@ -49,74 +50,19 @@ app.post('/webhook/', function(req, res) {
     res.sendStatus(200)
 })
 
-function sendTextMessage(sender, text) {
-    var messageData = {
-        text: text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+app.get('/emotion/:emotion', function(req, res) {
+    res.send("req:", req.params)
+    sendGifs(req.params.emotion)
+})
+
+
+
+function dealWithMessage() {
+
 }
 
-function sendGenericMessage(sender) {
-    var messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+function getGifData(emotion) {
+
 }
 
 function sendGifs(emotion) {
@@ -124,21 +70,16 @@ function sendGifs(emotion) {
 
         var elements = []
 
-        for (var i = 0; i < 3; i++) {
+        for (var i = 0; i < 5; i++) {
             
             var result = res.data[i]
-            console.log(i)
-            console.log(result)
-
             elements.push({
                 "title": "title:"+ result.source_tld,
                 "image_url": result.images.original.url
             })
         }
 
-        console.log("elements: ", elements)
-
-        var messageData = {
+        var messageObject = {
             "attachment": {
                 "type": "template",
                 "payload": {
@@ -148,25 +89,29 @@ function sendGifs(emotion) {
             }
         }
 
-        console.log("WITNESS ME")
-        console.log("messageData: ", messageData)
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: token },
-            method: 'POST',
-            json: {
-                recipient: { id: sender },
-                message: messageData,
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-        })
+        postMessage(messageObject)
     });
+}
+
+function postMessage(messageObject) {
+
+    console.log("sendMessage: ", messageData)
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
 
 // Spin up the server
